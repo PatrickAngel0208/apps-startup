cbfcc896840e87bcb9226ed44e8b83e34394f3bc
Carlos Angel Patricio Martinez <54094661+PatrickAngel0208@users.noreply.github.com>
2023-01-14 19:48:16 -0600

    Main code was added.

---
+++ main-code.bat
@@ -0,0 +1,45 @@
+@ECHO OFF 
+
+:: This batch starts DokWatcher and ScanManager Apps.
+
+set source_folder=%CD%
+set dest_folder=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+set cmlab_folder=C:\Windows\System32
+
+:: ECHO %source_folder%
+:: ECHO %dest_folder%
+:: ECHO %cmlab_folder%
+ 
+cd %dest_folder%
+if exist CMLab.bat (
+
+	cd %cmlab_folder%
+ 
+	if exist dokwatcher.exe (
+		if exist ScanManager.exe (
+			START	dokwatcher
+			START	scanmanager
+		)
+	) else (
+
+		ECHO ==================================
+		ECHO CMLab tools were not found.
+		ECHO Please contact to GDC CMLab Leader.
+		ECHO ==================================
+		PAUSE
+)
+) else (
+	ECHO =============================================================
+	ECHO.
+	ECHO Kindly wait... The necessary file is currently being copied.
+	ECHO.
+	copy "%source_folder%\CMLab.bat" "%dest_folder%" 
+	START	dokwatcher
+	START	scanmanager
+	ECHO.
+	ECHO Thank you for your patience; you are now free to remove me.
+	ECHO.
+	ECHO =============================================================
+	PAUSE
+)
+
